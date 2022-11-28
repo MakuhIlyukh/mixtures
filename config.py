7b8bd85c93c8d572667e06d31fca77b7c5c20329
Makukha_Ilia <makukha.it@gmail.com>
2022-11-28 10:43:40 +0300

    added mlflow loggin for train

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 DATASETS_ARTIFACTS_PATH = "data/datasets"
+TRAINED_MODELS_PATH = "data/trained_models"
+TRAIN_PLOTS_PATH = "data/trained_models/plots"
 
 # tags for mlflow
 # keys
 DATASETS_TAG_KEY = "datasets"
 MODELS_TAG_KEY = "models"
+FAST_TAG_KEY = "fast"
 
 # values
 GENERATION_TAG_VALUE = "generation"
 TRAINING_TAG_VALUE = "training"
+FAST_TAG_VALUE = "fast"
--- train.py
+++ train.py
@@ -1,55 +1,118 @@
 # %%
+from os.path import join as joinp
+
 import torch
 from torch import nn
 from tqdm import tqdm
 from sklearn.preprocessing import MinMaxScaler
+import mlflow
+import matplotlib.pyplot as plt
 
 from src.datasets import (
-    load_dataset, GaussianMixtureSampler, scatter)
+    load_dataset, GaussianMixtureSampler)
+from src.plotting import (
+    scatter, gm_plot)
 from src.models import GM
 from src.losses import nll
+from config import (
+    DATASETS_ARTIFACTS_PATH as DAP,
+    TRAINED_MODELS_PATH as TRMP,
+    TRAIN_PLOTS_PATH as TPP)
 
 
-def train(X, gm, n_epochs, optimizer, loss_fn):
+TRAIN_SEED = 106
+LR = 10**(-5)
+MIN_MAX_SCALING = False
+N_EPOCHS = 8*10**3
+WEIGHT_DECAY = 0.0
+OPTIMIZER = "SGD"
+LOSS_PREFIX = "NLL"
+PLOT_EVERY = 100
+
+
+def train(X, gm, n_epochs, optimizer, loss_fn,
+          loss_prefix="", plot_every=100):
+    # loss_name is used for logging
+    loss_name = loss_prefix + "_loss"
+    X_numpy = X
     X = torch.from_numpy(X)
+    # TODO: add batch splitting
     tqdm_bar = tqdm(range(n_epochs))
-    for epoch in tqdm(tqdm_bar):
+    for epoch in tqdm_bar:
         optimizer.zero_grad()
         dens = gm(X)
         loss = loss_fn(dens)
         loss.backward()
         optimizer.step()
-        tqdm_bar.set_postfix({"loss": loss.item()})
+
+        tqdm_bar.set_postfix({
+            loss_name: loss.item()})
+
+        if epoch % plot_every == 0:
+            fig, ax = plt.subplots()
+            scatter(X_numpy, y, axis=ax)
+            gm_plot(
+                gm.m_w.detach().numpy(),
+                gm.s_w(no_grad=True).numpy(),
+                axis=ax)
+            fig.savefig(joinp(TPP, f"gm_plot_{epoch}.png"))
+            plt.close(fig)  # ???: может лучше очищать фигуру и создавать не в цикле?
         
+        mlflow.log_metric(loss_name, loss.item(), step=epoch)
+
 
 if __name__ == '__main__':
-    torch.manual_seed(106)
+    # mlflow starting, params logging
+    train_run = mlflow.start_run()
+    mlflow.log_params({
+        "TRAIN_SEED": TRAIN_SEED,
+        "LR": LR,
+        "MIN_MAX_SCALING": MIN_MAX_SCALING,
+        "N_EPOCHS": N_EPOCHS,
+        "WEIGHT_DECAY": WEIGHT_DECAY,
+        "OPTIMIZER": OPTIMIZER,
+        "LOSS": LOSS_PREFIX})
 
-    with open("data/gms.pkl", 'rb') as f:
+    # setting seed of torch
+    torch.manual_seed(TRAIN_SEED)
+
+    # loading dataset
+    with open(joinp(DAP, "gms.pkl"), 'rb') as f:
         gm_sampler = GaussianMixtureSampler.load(f)
-    with open("data/Xy.pkl", 'rb') as f:
+    with open(joinp(DAP, "Xy.pkl"), 'rb') as f:
         X, y = load_dataset(f)
     k = gm_sampler.k
     d = gm_sampler.d
 
+    # creating a model
     gm = GM(k, d)
-    optimizer = torch.optim.Adam(
+
+    # choosing an optimizer
+    optimizer = torch.optim.SGD(
         params=gm.parameters(),
-        lr=10**(-3),
-        weight_decay=0.0)
+        lr=LR,
+        weight_decay=WEIGHT_DECAY)
+    
+    # preprocessing steps
+    if MIN_MAX_SCALING:
+        scaler = MinMaxScaler()
+        X_proc = scaler.fit_transform(X)
+        # TODO: apply scaler to gm.m_w and gm.s_w (gm.l_w)
+        # TODO: добавь константую инициализацию пресижона 
+        #       (EYE * alpha, константа определяется из датасета)
+    else:
+        X_proc = X
+
+    # training
     loss_fn = nll
-    n_epochs = 8*10**3
-    # X_proc = MinMaxScaler().fit_transform(X)
-    X_proc = X.copy()
+    train(X_proc, gm, N_EPOCHS, optimizer, loss_fn,
+          loss_prefix=LOSS_PREFIX, plot_every=PLOT_EVERY)
 
-    train(X_proc, gm, n_epochs, optimizer, loss_fn)
-
-
-# %%
-# optimizer = torch.optim.Adam(
-#     params=gm.parameters(),
-#     lr=10**(-1),
-#     weight_decay=0.0)
-n_epochs = 10**3
-
-train(X_proc, gm, n_epochs, optimizer, loss_fn)
+    # TODO: artifacts logging
+    # TODO: Обрати внимание на то, чтобы папка plots очищалась
+    #       перед запуском, ибо иначе будут логироваться данные с прошлых
+    #       запусков. А может должна очищаться не только папка plots?
+    #       А может вообще нужно, чтобы файлы не сохранялись в папки,
+    #       а сразу заносились в mlflow runs?
+    mlflow.log_artifact(TPP)
+    # mlflow.pytorch.log_model(gm, TRMP)
